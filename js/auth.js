cfc33504f1582d9912f8ccc2d92cc12fb599ac6e
Rodolfo Mora <rodolfomora97@outlook.com>
2024-09-25 21:07:50 -0600

    login consumiendo api

---
+++ js/auth.js
@@ -0,0 +1,12 @@
+
+// Función para verificar si el token existe en localStorage
+function checkAuthToken() {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        alert("No tienes acceso. Por favor, inicia sesión.");
+        window.location.href = 'login.html';
+    } else {
+        console.log("Acceso concedido, token encontrado.");
+    }
+}
